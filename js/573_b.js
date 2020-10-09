9132df581313e5a8be35edb52c3c38d411dfa147
alxdrdelaporte <62095043+alxdrdelaporte@users.noreply.github.com>
2020-10-09 12:22:22 +0200

    Add 573_b.js

---
+++ js/573_b.js
@@ -0,0 +1,24 @@
+var konami = ['ArrowUp', 'ArrowUp', 'ArrowDown', 'ArrowDown', 'ArrowLeft', 'ArrowRight', 'ArrowLeft', 'ArrowRight', 'b', 'a'];
+var progress = 0;
+
+var keyHandler = function(event){
+	if (konami.indexOf(event.key) < 0 || event.key !== konami[progress]) {
+		progress = 0;
+		return;
+	}
+	progress++;
+	if (konami.length === progress) {
+		progress = 0;
+		var mystyle = document.getElementById("maincss");
+		var stylecheck = mystyle.href.endsWith("alxdr1.css");
+		var congrats = document.getElementById("konamicongrats");
+		if (stylecheck == true) {
+			mystyle.setAttribute("href", "../alxdr2.css");
+			congrats.setAttribute("style", "display: block");
+		} else {
+			mystyle.setAttribute("href", "../alxdr1.css");
+			congrats.setAttribute("style", "display: none");
+		}
+	}
+};
+document.addEventListener('keydown', keyHandler, false);
