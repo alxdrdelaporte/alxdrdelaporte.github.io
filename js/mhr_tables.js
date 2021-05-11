b2b8222d58a4ca08e1153d2bfa7510485189e7f9
alxdrdelaporte <62095043+alxdrdelaporte@users.noreply.github.com>
2021-05-11 17:45:59 +0200

    Upload MHR project files
    HTML and JS

---
+++ js/mhr_tables.js
@@ -0,0 +1,149 @@
+var distable = $('#tab_distinctions').DataTable( {
+    language: {
+        processing:     "Traitement en cours...",
+        search:         "Rechercher&nbsp;:",
+        lengthMenu:    "Afficher _MENU_ distinctions",
+        info:           "Affichage des distinctions _START_ à _END_ sur _TOTAL_",
+        infoEmpty:      "Affichage des distinctions _START_ à _END_ sur _TOTAL_",
+        infoFiltered:   "(filtré de _MAX_ éléments au total)",
+        infoPostFix:    "",
+        loadingRecords: "Chargement en cours...",
+        zeroRecords:    "Aucune distinction à afficher",
+        emptyTable:     "Aucune donnée disponible dans le tableau",
+        paginate: {
+            first:      "Premier",
+            previous:   "Précédent",
+            next:       "Suivant",
+            last:       "Dernier"
+        },
+        aria: {
+            sortAscending:  ": activer pour trier la colonne par ordre croissant",
+            sortDescending: ": activer pour trier la colonne par ordre décroissant"
+        }
+    },
+    "search": {
+        "regex": true
+    },
+    "pagingType": "full_numbers"
+}
+ );
+
+var perstable = $('#tab_personnages').DataTable( {
+    language: {
+        processing:     "Traitement en cours...",
+        search:         "Rechercher&nbsp;:",
+        lengthMenu:    "Afficher _MENU_ personnages",
+        info:           "Affichage des personnages _START_ à _END_ sur _TOTAL_",
+        infoEmpty:      "Affichage des personnages _START_ à _END_ sur _TOTAL_",
+        infoFiltered:   "(filtré de _MAX_ personnages au total)",
+        infoPostFix:    "",
+        loadingRecords: "Chargement en cours...",
+        zeroRecords:    "Aucun personnage à afficher",
+        emptyTable:     "Aucune donnée disponible dans le tableau",
+        paginate: {
+            first:      "Premier",
+            previous:   "Précédent",
+            next:       "Suivant",
+            last:       "Dernier"
+        },
+        aria: {
+            sortAscending:  ": activer pour trier la colonne par ordre croissant",
+            sortDescending: ": activer pour trier la colonne par ordre décroissant"
+        }
+    },
+    "search": {
+        "regex": true
+    },
+    "pagingType": "full_numbers"
+}
+ );
+
+var bidultable = $('#tab_bidulons').DataTable( {
+    language: {
+        processing:     "Traitement en cours...",
+        search:         "Rechercher&nbsp;:",
+        lengthMenu:    "Afficher _MENU_ bidulons",
+        info:           "Affichage des bidulons _START_ à _END_ sur _TOTAL_",
+        infoEmpty:      "Affichage des bidulons _START_ à _END_ sur _TOTAL_",
+        infoFiltered:   "(filtré de _MAX_ bidulons au total)",
+        infoPostFix:    "",
+        loadingRecords: "Chargement en cours...",
+        zeroRecords:    "Aucun bidulon à afficher",
+        emptyTable:     "Aucune donnée disponible dans le tableau",
+        paginate: {
+            first:      "Premier",
+            previous:   "Précédent",
+            next:       "Suivant",
+            last:       "Dernier"
+        },
+        aria: {
+            sortAscending:  ": activer pour trier la colonne par ordre croissant",
+            sortDescending: ": activer pour trier la colonne par ordre décroissant"
+        }
+    },
+    "search": {
+        "regex": true
+    },
+    "pagingType": "full_numbers"
+}
+ );
+
+var mervtable = $('#tab_merveilloux').DataTable( {
+    language: {
+        processing:     "Traitement en cours...",
+        search:         "Rechercher&nbsp;:",
+        lengthMenu:    "Afficher _MENU_ merveilloux",
+        info:           "Affichage des merveilloux _START_ à _END_ sur _TOTAL_",
+        infoEmpty:      "Affichage des merveilloux _START_ à _END_ sur _TOTAL_",
+        infoFiltered:   "(filtré de _MAX_ merveilloux au total)",
+        infoPostFix:    "",
+        loadingRecords: "Chargement en cours...",
+        zeroRecords:    "Aucun merveillou à afficher",
+        emptyTable:     "Aucune donnée disponible dans le tableau",
+        paginate: {
+            first:      "Premier",
+            previous:   "Précédent",
+            next:       "Suivant",
+            last:       "Dernier"
+        },
+        aria: {
+            sortAscending:  ": activer pour trier la colonne par ordre croissant",
+            sortDescending: ": activer pour trier la colonne par ordre décroissant"
+        }
+    },
+    "search": {
+        "regex": true
+    },
+    "pagingType": "full_numbers"
+}
+ );
+
+var poisstable = $('#tab_poissons').DataTable( {
+    language: {
+        processing:     "Traitement en cours...",
+        search:         "Rechercher&nbsp;:",
+        lengthMenu:    "Afficher _MENU_ poissons",
+        info:           "Affichage des poissons _START_ à _END_ sur _TOTAL_",
+        infoEmpty:      "Affichage des poissons _START_ à _END_ sur _TOTAL_",
+        infoFiltered:   "(filtré de _MAX_ poissons au total)",
+        infoPostFix:    "",
+        loadingRecords: "Chargement en cours...",
+        zeroRecords:    "Aucun poisson à afficher",
+        emptyTable:     "Aucune donnée disponible dans le tableau",
+        paginate: {
+            first:      "Premier",
+            previous:   "Précédent",
+            next:       "Suivant",
+            last:       "Dernier"
+        },
+        aria: {
+            sortAscending:  ": activer pour trier la colonne par ordre croissant",
+            sortDescending: ": activer pour trier la colonne par ordre décroissant"
+        }
+    },
+    "search": {
+        "regex": true
+    },
+    "pagingType": "full_numbers"
+}
+ );
